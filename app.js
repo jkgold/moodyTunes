1b3873833dac3c3a9ba2526dfe5cf2b55740b30b
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2016-10-31 15:31:23 -0600

    if user imput is blank returned yo mama

--- app.js
+++ app.js
@@ -2,3 +2,11 @@
     $(document).ready(function(){
       $('.parallax').parallax();
     });
+$('button').on('click', function(event){
+  event.preventDefault();
+  var $userInput = $('input');
+  if ($userInput.val()===""){
+    console.log("Yo Mama");
+    return;
+  }
+})
