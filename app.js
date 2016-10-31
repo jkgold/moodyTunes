9444b3f6a650d788414db8eae451dfcaba9f8bcb
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2016-10-31 16:26:47 -0600

    got spotify to return an object woohoo

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 
     $(document).ready(function(){
-      $('.parallax').parallax();
-    });
-$('button').on('click', function(event){
-  event.preventDefault();
-  var $userInput = $('input');
-  if ($userInput.val()===""){
-    console.log("Yo Mama");
-    return;
-  }
-})
+      // $('.parallax').parallax();
+
+      $('#btn').on('click', function(event){
+        event.preventDefault()
+        console.log('clicked!!');
+        event.preventDefault();
+        var $userInput = $('input');
+        if ($userInput.val()===""){
+          console.log("Yo Mama");
+          return;
+        }
+        var $userApi = "https://api.spotify.com/v1/search?q=radiohead%20&type=artist";
+        // // $userApi += $userInput.val();
+        $.getJSON($userApi)
+        .then(function(data){
+          console.log('data=======',data);
+        })
+v
+      })
+});
