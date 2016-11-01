53370c09c6acb8d90b15bbaad8dd10105de60bfa
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2016-11-01 13:18:39 -0600

    Passing closer information

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 
     $(document).ready(function(){
       // $('.parallax').parallax();
-
+      // var Spotify = require('spotify-web-api-js');
+      // var s = new Spotify();
+      // var spotifyApi = new SpotifyWebApi();
+      // spotifyApi.setAccessToken('<5725c2b0a5e24169b8d5381c4e17af67>');
       $('#btn').on('click', function(event){
-        event.preventDefault()
-        console.log('clicked!!');
         event.preventDefault();
-        var $userInput = $('input');
-        if ($userInput.val()===""){
-          console.log("Yo Mama");
-          return;
-        }
-        var $userApi = "https://api.spotify.com/v1/search?q=radiohead%20&type=artist";
+
+        // console.log('clicked!!');
+        // event.preventDefault();
+        // var $userInput = $('input');
+        // if ($userInput.val()===""){
+        //   console.log("Yo Mama");
+        //   return;
+        // }
+        // function multiWords(userName){
+        var userInput = $("#query").val();
+        for (var i = 0; i < userInput.length; i++){
+          if(userInput[i] === " "){
+            console.log(userInput.split(' ').join("+"));
+          }
+
+          }
+        // }
+        // console.log("user input;", userInput);
+        var $userApi = "https://api.spotify.com/v1/search?q=" +`${userInput}` + "%20&type=artist";
+        console.log($userApi);
+        // var $userApi = "https://api.spotify.com/v1/albums/'0ADkBHZhR2cVfANgK5gHQO'/tracks";
         // // $userApi += $userInput.val();
         $.getJSON($userApi)
         .then(function(data){
-          console.log('data=======',data);
+          // console.log('data=======',data);
         })
-v
+
       })
 });
