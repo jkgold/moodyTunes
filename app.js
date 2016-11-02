8628155d7789e55ed0e79de27c65233bad850271
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2016-11-02 16:56:08 -0600

    Cleaned up and functional

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 
     $(document).ready(function(){
-      // $('.parallax').parallax();
-
-
       $('#btn').on('click', function(event){
         event.preventDefault();
 
         function addElement(){
           var newDiv = document.createElement("iframe");
           $(".inner").append(newDiv);
-          newDiv.setAttribute("src", "https://embed.spotify.com/?uri=/open.spotify.com/artist/3NZE8jDD2cCzd4vUyZ5CQQ");
+          newDiv.setAttribute("src", "https://embed.spotify.com/?uri=/open.spotify.com/artist/");
           console.log(newDiv);
         }
         addElement();
@@ -32,9 +29,7 @@
         var itemsValue = data;
         var topFive = itemsValue.artists.items[0].external_urls;
         var playlistTopFive = topFive.spotify;
-        // console.log(playlistTopFive);
-      //    var stringList = JSON.stringify(playlistTopFive);
-      //    console.log(stringList);
+
        console.log(playlistTopFive);
        var removeUrl = 'https://open.spotify.com/artist/';
        var returnKey =
@@ -47,52 +42,7 @@
       //  console.log(returnKey);
       }
       // cutOffUrl();
-
-      //  'https://embed.spotify.com/?uri=/open.spotify.com/artist/3NZE8jDD2cCzd4vUyZ5CQQ'.replace(removeUrl,'')
-      //  "3NZE8jDD2cCzd4vUyZ5CQQ"
-
+        })
 
       })
-
-      })
-});
-
-
-
-//     $(document).ready(function(){
-//       // $('.parallax').parallax();
-//       // var Spotify = require('spotify-web-api-js');
-//       // var s = new Spotify();
-//       // var spotifyApi = new SpotifyWebApi();
-//       // spotifyApi.setAccessToken('<5725c2b0a5e24169b8d5381c4e17af67>');
-//       //     "target_uri": "spotify:track:3SVAN3BRByDmHOhKyIDxfC",
-//
-//       $('#btn').on('click', function(event){
-//         event.preventDefault();
-//
-//
-//         var userInput = $("#query").val();
-//         for (var i = 0; i < userInput.length; i++){
-//           if(userInput[i] === " "){
-//             userInput = userInput.split(' ').join("+");
-//
-//           }
-//
-//           }
-//
-//
-//         var $userApi = "https://api.spotify.com/v1/search?q=" + userInput + "%20&type=artist";
-//         console.log($userApi);
-//         // console.log($('external_urls:').val());
-//         // console.log($userApi.artists);
-//
-//           $.getJSON($userApi)
-//         .then(function(data){
-//           // var itemsValue = data;
-//           console.log(itemsValue.external_urls);
-//           // console.log($("external_urls"));
-//
-//         })
-//
-//       })
-// });
+  });
