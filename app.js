86b4d33676e3b3e105ea43422a6b85878ea17741
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2016-11-01 18:07:35 -0600

    isolated the external url

--- app.js
+++ app.js
@@ -22,17 +22,61 @@
 
 
         var $userApi = "https://api.spotify.com/v1/search?q=" + userInput + "%20&type=artist";
-        console.log($userApi);
+        // console.log($userApi);
         // console.log($('external_urls:').val());
         // console.log($userApi.artists);
 
           $.getJSON($userApi)
         .then(function(data){
-          var itemsValue = data.artists.items;
-          // console.log(itemsValue.external_urls);
-          console.log($("external_urls"));
+          var itemsValue = data;
+          // console.log(itemsValue.artists);
+          // console.log(itemsValue.artists.items);
+          var topFive = itemsValue.artists.items[0].external_urls;
+          console.log(topFive);
+
+
 
         })
 
       })
 });
+
+
+
+//     $(document).ready(function(){
+//       // $('.parallax').parallax();
+//       // var Spotify = require('spotify-web-api-js');
+//       // var s = new Spotify();
+//       // var spotifyApi = new SpotifyWebApi();
+//       // spotifyApi.setAccessToken('<5725c2b0a5e24169b8d5381c4e17af67>');
+//       //     "target_uri": "spotify:track:3SVAN3BRByDmHOhKyIDxfC",
+//
+//       $('#btn').on('click', function(event){
+//         event.preventDefault();
+//
+//
+//         var userInput = $("#query").val();
+//         for (var i = 0; i < userInput.length; i++){
+//           if(userInput[i] === " "){
+//             userInput = userInput.split(' ').join("+");
+//
+//           }
+//
+//           }
+//
+//
+//         var $userApi = "https://api.spotify.com/v1/search?q=" + userInput + "%20&type=artist";
+//         console.log($userApi);
+//         // console.log($('external_urls:').val());
+//         // console.log($userApi.artists);
+//
+//           $.getJSON($userApi)
+//         .then(function(data){
+//           // var itemsValue = data;
+//           console.log(itemsValue.external_urls);
+//           // console.log($("external_urls"));
+//
+//         })
+//
+//       })
+// });
